135290f5cd6d7efe5f2a7dafc72264d8cc50e300
naruepol <naruapon@gmail.com>
2023-02-26 21:41:49 +0700

    Update EncryptType1.php

--- EncryptType1.php
+++ EncryptType1.php
@@ -4,7 +4,7 @@ class EncryptType1 implements EncryptAlgorithm
 {
     public function encrypt($pwd)
     {
-        $pwd_encypt = password_hash("$pwd", PASSWORD_DEFAULT);
+        $pwd_encypt = password_hash($pwd, PASSWORD_DEFAULT);
         return $pwd_encypt;
     }
     public function verify($pwd, $epwd)
